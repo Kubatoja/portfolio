99dfb13097bd4debab9da084130ef3c8b47886f6
Kubatoja <jakub1720@gmail.com>
2023-12-22 00:20:05 +0100

    ww

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.scss";
-import Background from "./components/background";
+import Background from "./components/Background";
 import Navbar from "./components/navbar";
 import Profile from "./components/Profile";
 import Projects from "./components/Projects";
