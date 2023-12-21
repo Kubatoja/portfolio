9ba63d2899205a6c2e11aa8d7502d1ab66937195
Kubatoja <jakub1720@gmail.com>
2023-12-22 00:15:56 +0100

    ww
    ww

--- portfolio/src/components/test.jsx
+++ portfolio/src/components/test.jsx
@@ -2,54 +2,7 @@ import React, { useEffect, useState } from "react";
 import "../styles/projects.scss";
 
 function test() {
-  const [activeCard, setActiveCard] = useState(0);
-
-  function card() {
-    document.getElementsByClassName("active")[0].classList.remove("active");
-
-    document.getElementsByClassName("card")[activeCard].classList.add("active");
-  }
-  useEffect(() => {
-    card();
-  });
-  return (
-    <div className="test">
-      <h1>Ostatnie projekty</h1>
-      <div className="testContainer">
-        <div
-          className="card active"
-          onClick={() => {
-            setActiveCard(0);
-          }}
-        >
-          <div className="image aimeth"></div>
-          <div className="text ">
-            <p>AI-Methods</p>LOREM IPSUM LOREM IPSUM LOREM IPSUM LOREM IPSUM
-            LOREM IPSUM LOREM IPSUM LOREM IPSUM LOREM IPSUM LOREM IPSUM LOREM
-            IPSUM LOREM IPSUM{" "}
-          </div>
-        </div>
-        <div
-          className="card"
-          onClick={() => {
-            setActiveCard(1);
-          }}
-        ></div>
-        <div
-          className="card"
-          onClick={() => {
-            setActiveCard(2);
-          }}
-        ></div>
-        <div
-          className="card"
-          onClick={() => {
-            setActiveCard(3);
-          }}
-        ></div>
-      </div>
-    </div>
-  );
+  return <div className="test"></div>;
 }
 
 export default test;
